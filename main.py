95a27d112574628d817aceb67e320bbad092c562
Thex <152274492+Thex-Thex@users.noreply.github.com>
2026-01-19 21:37:04 +0100

    adding a scan code fonction

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello word")
+# function that wait an input of code scan or manual
+def ScanCode():
+    code = input("Scan a code : ")
+    return code
+
+ScannedCode = ScanCode()
+print(ScannedCode)
