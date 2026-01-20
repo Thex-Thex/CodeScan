dd083f8531a5ac9815d635e2f7ce2ac2d688735c
Thex <152274492+Thex-Thex@users.noreply.github.com>
2026-01-20 21:12:26 +0100

    adding ask to save the code you scanned

--- main.py
+++ main.py
@@ -12,9 +12,21 @@ def scan_code():
     code = input("Scan a code or enter it manually : ")
     return code
 
+def save_code(codedata):
+    print("code saved")
 
 # Création du fichier CSV
 create_csv_file()
 
+# calling fonction to save code
 ScannedCode = scan_code()
 print(ScannedCode)
+
+# asking to save code while reply not good : yes or no
+while True: 
+    savecode = input("Save code y or n : ")
+    if savecode == "y":
+        print("save")
+        break
+    elif savecode == "n":
+        break
