708c3f2f0581bc809d2017e90c024b61d9964902
Thex <152274492+Thex-Thex@users.noreply.github.com>
2026-01-20 20:54:11 +0100

    adding creation of csv file

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import csv
+
+
+def create_csv_file():
+    with open("codes.csv", mode="w", newline="", encoding="utf-8") as fichier:
+        writer = csv.writer(fichier)
+        writer.writerows(["Code"])
+    print("info : CSV file created successfully")
+
 # function that wait an input of code scan or manual
-def ScanCode():
-    code = input("Scan a code : ")
+def scan_code():
+    code = input("Scan a code or enter it manually : ")
     return code
 
-ScannedCode = ScanCode()
+
+# Création du fichier CSV
+create_csv_file()
+
+ScannedCode = scan_code()
 print(ScannedCode)
