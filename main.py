c1aad61f7acae97bef2df3d9e7367d7ef5ddea5c
Thex <152274492+Thex-Thex@users.noreply.github.com>
2026-01-20 21:35:05 +0100

    adding while to chain scan and add code to csv file

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def scan_code():
     return code
 
 
-def save_code(code_type, data):
+def save_code_to_csv(code_type, data):
     with open("codes.csv", mode="a", newline="", encoding="utf-8") as fichier:
         writer = csv.writer(fichier)
         writer.writerow(["1", code_type, data])
@@ -29,20 +29,21 @@ def save_code(code_type, data):
 # Création du fichier CSV
 create_csv_file(CsvColumn)
 
-# calling fonction to save code
-ScannedCode = scan_code()
-print(ScannedCode)
-
-####################
-# test variable
-code_type = "barcode"
-####################
-
-# asking to save code while reply not good : yes or no
 while True:
-    SaveCode = input("Save code y or n : ")
-    if SaveCode == "y":
-        save_code(code_type, ScannedCode)
-        break
-    elif SaveCode == "n":
-        break
+    # calling fonction to save code
+    ScannedCode = scan_code()
+    print(ScannedCode)
+
+    ####################
+    # test variable
+    code_type = "barcode"
+    ####################
+
+    # asking to save code while reply not good : yes or no
+    while True:
+        SaveCode = input("Save code y or n : ")
+        if SaveCode == "y":
+            save_code_to_csv(code_type, ScannedCode)
+            break
+        elif SaveCode == "n":
+            break
