e04957d5ca799b5bcf5b9e8b172cf89c12cb9278
Thex <152274492+Thex-Thex@users.noreply.github.com>
2026-01-20 21:31:27 +0100

    adding saving code to scv function

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import csv
 
+# csv Column
+CsvColumn = [
+    ["ID", "Type", "data"],
+    ["0", "Barcode", "123456"],
+]
 
-def create_csv_file():
+
+def create_csv_file(Column):
     with open("codes.csv", mode="w", newline="", encoding="utf-8") as fichier:
         writer = csv.writer(fichier)
-        writer.writerows(["Code"])
+        writer.writerows(Column)
     print("info : CSV file created successfully")
+
 
 # function that wait an input of code scan or manual
 def scan_code():
     code = input("Scan a code or enter it manually : ")
     return code
 
-def save_code(codedata):
-    print("code saved")
+
+def save_code(code_type, data):
+    with open("codes.csv", mode="a", newline="", encoding="utf-8") as fichier:
+        writer = csv.writer(fichier)
+        writer.writerow(["1", code_type, data])
+
 
 # Création du fichier CSV
-create_csv_file()
+create_csv_file(CsvColumn)
 
 # calling fonction to save code
 ScannedCode = scan_code()
 print(ScannedCode)
 
+####################
+# test variable
+code_type = "barcode"
+####################
+
 # asking to save code while reply not good : yes or no
-while True: 
-    savecode = input("Save code y or n : ")
-    if savecode == "y":
-        print("save")
+while True:
+    SaveCode = input("Save code y or n : ")
+    if SaveCode == "y":
+        save_code(code_type, ScannedCode)
         break
-    elif savecode == "n":
+    elif SaveCode == "n":
         break
